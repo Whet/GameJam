a40dfed01c5d63bf8d6090e8976223678da901cf
Charles Sherman <shermzor@gmail.com>
2015-01-24 17:04:03 +0000

    Changed font

--- src/watoydoEngine/fonts/FontList.java
+++ src/watoydoEngine/fonts/FontList.java
@@ -14,7 +14,7 @@ public class FontList {
 		Font dynamicFont = null;
 	    
 		try {
-			dynamicFont = Font.createFont(Font.TRUETYPE_FONT, ReadWriter.getResourceAsInputStream("fonts/LobsterTwo-Regular.ttf"));
+			dynamicFont = Font.createFont(Font.TRUETYPE_FONT, ReadWriter.getResourceAsInputStream("fonts/Chunkfive.ttf"));
 		} catch (FontFormatException | IOException e) {
 			e.printStackTrace();
 			System.exit(-1);

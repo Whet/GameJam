a6a8aac4e55d16c340cc2af9fa308641abb289ba
Charles Sherman <shermzor@gmail.com>
2015-01-25 15:37:32 +0000

    fix to npe

--- src/game/audio/AudioHandler.java
+++ src/game/audio/AudioHandler.java
@@ -16,17 +16,20 @@ public class AudioHandler {
 	//move later
 	public void playAudio(String audio) {
 
-		
+		try {
 		ac.out.clearInputConnections();
 		//Stream ting
 		//SampleManager.setBufferingRegime(Sample.Regime.newStreamingRegime(1000));
-		SamplePlayer player = new SamplePlayer(ac, SampleManager
-				.sample(audio));
+		SamplePlayer player = new SamplePlayer(ac, SampleManager.sample(audio));
 		Gain g = new Gain(ac, 2, 0.2f);
 		g.addInput(player);
 		ac.out.addInput(g);
 		System.out.println("playing audio");
 		ac.start();
+		}
+		catch(NullPointerException e) {
+			
+		}
 	}
 	
 	public void playNothing()
